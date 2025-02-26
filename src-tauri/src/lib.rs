d7878ef7780187684dca0dcb8fe96a722c0da6d2
ayushch80 <ayushch80@gmail.com>
2025-02-27 01:09:08 +0530

    feat: enable hostname-based ping for Raspberry Pi

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,34 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use std::process::Command;
+use regex::Regex;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn find_ip(hostname: &str, os: &str) -> String {
+    let mut ip = String::new();
+    let ipv4_regex = Regex::new(r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b").unwrap();
+    if os == "linux" {
+        let mut cmd = Command::new("ping");
+        let res = cmd.args([hostname, "-c", "1"]).output().unwrap();
+
+        let stderr = String::from_utf8(res.clone().stderr);
+        let stdout = String::from_utf8(res.clone().stdout);
+
+        if stderr != Ok(String::from("")) {
+            return ip;
+        }
+
+        if let Some(cap) = ipv4_regex.find(stdout.clone().expect("Failed").as_str()) {
+            ip = cap.as_str().to_string();
+        }
+    }
+    return ip;
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![find_ip])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
