9253847f24e51786e370f3495122cefa05b549d1
ayushch80 <ayushch80@gmail.com>
2025-02-28 18:01:48 +0530

    feat(StatusBar): add navigation buttons and improve layout

--- src/components/StatusBar.svelte
+++ src/components/StatusBar.svelte
@@ -1,14 +1,63 @@
 <script lang="ts">
     import { platform, type Platform } from "@tauri-apps/plugin-os";
+    import { IconChevronLeft, IconChevronRight, IconHome } from "@tabler/icons-svelte";
+    import { goto } from "$app/navigation";
 
     let os : Platform = platform();
+
+    function goBack() {
+        history.back();
+    }
+    function goForeward() {
+        history.forward();
+    }
+    function goHome() {
+        goto('/')
+    }
 </script>
 
 <nav>
+    <button on:click={goBack}><IconChevronLeft color="#ccc" /></button>
+    <button on:click={goForeward}><IconChevronRight color="#ccc" /></button>
+    <button on:click={goHome}><IconHome color="#ccc" /></button>
+    <span style="flex: 1;">{""}</span>
     <span class="highlighted">OS: <code>{os}</code></span>
 </nav>
+<hr />
 
-<style>
+<style lang="css">
+    nav {
+        display: flex;
+        padding-top: 0.25em;
+        padding-left: 0.25em;
+        padding-right: 0.25em;
+
+        flex-direction: row;
+        align-content: center;
+
+        color: #ccc;
+    }
+    nav * {
+        margin-right: 0.25em;
+    }
+    button {
+        display: grid;
+        place-content: center;
+
+        background: rgba(255,255,255,0);
+        border: none;
+        border-radius: 0.25em;
+        cursor: pointer;
+
+        width: 3em;
+
+        text-decoration: none;
+
+        transition: all 0.125s ease;
+    }
+    button:hover {
+        background: rgba(255,255,255,0.2);
+    }
     .highlighted {
         background: #000;
         color: #fff;
