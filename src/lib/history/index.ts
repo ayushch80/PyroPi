76307f60f1c86dc547a3cb8a399baa09e5c603d4
ayushch80 <ayushch80@gmail.com>
2025-03-16 00:15:25 +0530

    wip(history): enhance history management with delete functions and state tracking

--- src/lib/history/index.ts
+++ src/lib/history/index.ts
@@ -16,9 +16,6 @@ interface ExtendedHistoryElement extends HistoryElement {
     id: number;
 }
 
-// Clear history if any when the app starts
-Dexie.delete("history");
-
 const history = new Dexie("history") as Dexie & {
     history: EntityTable<
         ExtendedHistoryElement,
@@ -26,23 +23,52 @@ const history = new Dexie("history") as Dexie & {
     >;
 };
 
+// Clear history if any when the app starts
+history.delete({ disableAutoOpen: false });
+
 history.version(version).stores({
     history: "++id, path, timestamp, states",
 });
 
-const addHistory = (path: string, states: object | any) => {
+const addHistory = async (
+    path: string,
+    states: object | any,
+): Promise<boolean> => {
     try {
-
-        history.history.add({
+        await history.history.add({
             path: path,
             timestamp: Date.now(),
             states: {
                 editSaved: states.editSaved,
                 selectedDevice: states.selectedDevice,
-                newDeviceAdded: states.newDeviceAdded
-            }
-        })
+                newDeviceAdded: states.newDeviceAdded,
+            },
+        });
+        return true;
     } catch (error) {
         console.error(error);
+        return false;
     }
 };
+
+const deleteAfterId = async (id: number): Promise<boolean> => {
+    try {
+        await history.history.where("id").above(id).delete();
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+};
+
+const deleteAfterIndex = async (index: number): Promise<boolean> => {
+    try {
+        return await deleteAfterId(index + 1);
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+};
+
+export type { ExtendedHistoryElement, HistoryElement };
+export { addHistory, deleteAfterId, deleteAfterIndex, history };
--- src/routes/reactiveVariables.svelte.ts
+++ src/routes/reactiveVariables.svelte.ts
@@ -1,9 +1,8 @@
-let editSaved: object | any = $state({value: false});
-let selectedDevice: object | any = $state({id: -1});
-let newDeviceAdded: object | any = $state({value: false});
+let editSaved: object | any = $state({ value: false });
+let selectedDevice: object | any = $state({ id: -1 });
+let newDeviceAdded: object | any = $state({ value: false });
 
-export {
-    editSaved,
-    selectedDevice,
-    newDeviceAdded
-}
+// To track the history
+let historyIndex: object | any = $state({ index: 0, id: 1 });
+
+export { editSaved, historyIndex, newDeviceAdded, selectedDevice };
